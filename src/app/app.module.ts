face15683f49e10e805b61f481d75d047615fa3b
TheLastWeaboo <johncarlregala05@gmail.com>
2024-09-19 21:31:47 +0800

    added calculator component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { CounterComponent } from './components/counter/counter.component';
 import { SimpleformComponent } from './components/simpleform/simpleform.component';
 import { UserageComponent } from './components/userage/userage.component';
 import { UsergreetingComponent } from './components/usergreeting/usergreeting.component';
+import { CalculatorComponent } from './components/calculator/calculator.component';
 
 
 
@@ -32,6 +33,7 @@ const routes: Routes = [
   { path: 'simpleform', component: SimpleformComponent },
   { path: 'userage', component: UserageComponent },
   { path: 'usergreeting', component: UsergreetingComponent },
+  { path: 'calculator', component: CalculatorComponent},
 
 ];
 
